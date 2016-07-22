3fbae989dc02fd1e201c79c78d0efce72a808939
Cihangir SAVAS <cihangir@koding.com>
2016-07-22 17:24:20 +0300

    packer: source all the sh files under env folder

---
+++ scripts/export_env_vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+source /etc/profile
+
+for i in $(dirname $0)/env/*.sh ; do
+    if [ -r "$i" ]; then
+        if [ "${-#*i}" != "$-" ]; then
+            . "$i"
+        else
+            . "$i" >/dev/null 2>&1
+        fi
+    fi
+done
