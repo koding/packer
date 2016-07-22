2ed5b313cff0c47ced9265200a497f2e030f1df9
Cihangir SAVAS <cihangir@koding.com>
2016-07-22 23:48:04 +0300

    packer/scripts/env: add new env vars

---
+++ scripts/env/aws.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+
+export AWS_REGION=${AWS_REGION:-us-east-1}
+export AWS_ACCESS_KEY=${AWS_ACCESS_KEY:-}
+export AWS_SECRET_KEY=${AWS_SECRET_KEY:-}
---
+++ scripts/env/java.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+
+export JAVA_HOME="/usr/java/latest"
+export PATH=$JAVA_HOME/bin:$PATH
