b4a75c4cb727b98eac561f25a1b265063d1402ee
Cihangir <cihangir@koding.com>
2016-08-15 09:46:29 +0300

    Update go version

--- scripts/env/go.sh
+++ scripts/env/go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
-export GO_VERSION="1.4.2"
+export GO_VERSION="1.6.3"
 export GO_TARBALL="go$GO_VERSION.linux-amd64.tar.gz"
 export GO_SRC_URL="https://storage.googleapis.com/golang/$GO_TARBALL"
