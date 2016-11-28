56a9d6c0ae44d72dd6a5e6497329803efbbc2cf7
Sonmez Kartal <sonmez@koding.com>
2016-11-28 23:28:05 +0000

    Add ppa:nginx/stable
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- test-ami/provision.sh
+++ test-ami/provision.sh
@@ -13,6 +13,8 @@ add-apt-repository -y ppa:chris-lea/node.js
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google.list
 
+add-apt-repository -y ppa:nginx/stable
+
 ## Update package repositories
 
 apt-get update
