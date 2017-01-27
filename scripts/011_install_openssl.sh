456f8c456c04f65ef273831e2a8c5e334fe9e047
Cihangir SAVAS <cihangir@koding.com>
2017-01-27 18:20:30 +0300

    packer: create openssl installer

---
+++ scripts/011_install_openssl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+
+source $(dirname $0)/export_env_vars.sh
+
+$YUM install openssl-devel
+
+pushd /tmp
+
+wget https://openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
+tar -zxvf openssl-$OPENSSL_VERSION.tar.gz -C /usr/local/src
+cd /usr/local/src/openssl-$OPENSSL_VERSION
+./config --prefix=/usr
+make depend
+make
+make install
+
+popd
---
+++ scripts/env/openssl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export OPENSSL_VERSION=${OPENSSL_VERSION:-"1.0.2h"}
