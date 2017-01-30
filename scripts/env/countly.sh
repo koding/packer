92c5c1cd53a16090da2090391a5133335cb7f965
Cihangir SAVAS <cihangir@koding.com>
2017-01-30 19:05:52 +0300

    packer/scripts: change url

--- scripts/env/countly.sh
+++ scripts/env/countly.sh
@@ -2,5 +2,5 @@
 
 export COUNTLY_VERSION=${COUNTLY_VERSION:-16.12.2}
 export COUNTLY_BUCKET=${COUNTLY_BUCKET:-koding-backups}
-export COUNTLY_S3_PATH=$COUNTLY_BUCKET/countly_enterprise_edition_$COUNTLY_VERSION.zip
+export COUNTLY_S3_PATH=$COUNTLY_BUCKET/countly-enterprise-edition-v$COUNTLY_VERSION.zip
 export COUNTLY_S3_LINK=${COUNTLY_S3_LINK:-$COUNTLY_S3_PATH}
