259070e90a01b993972f2c6bbe20b8338f6994b5
Cihangir SAVAS <cihangir@koding.com>
2017-01-30 19:48:09 +0300

    packer/scripts: move configuration to upper level then start building it

--- scripts/100_install_countly.sh
+++ scripts/100_install_countly.sh
@@ -16,7 +16,7 @@ service sendmail start
 
 aws s3 cp s3://$COUNTLY_S3_LINK countly.zip
 
-unzip countly.zip -d /opt/countly
+unzip countly.zip -d /opt
 
 cd /opt/countly
 
@@ -24,18 +24,10 @@ npm install -g node-gyp --unsafe-perm
 npm install -g grunt-cli --unsafe-perm
 npm install --unsafe-perm
 
-# install plugins
-node ./bin/scripts/install_plugins
-
-# install sdk-web
-./bin/scripts/update.sh sdk-web
-
-# compile scripts for production
-grunt dist-all
-
 #configure and start nginx
 useradd www-data
-cp /etc/nginx/conf.d/default.conf ./bin/config/nginx.default.backup
+# cp /etc/nginx/conf.d/default.conf ./bin/config/nginx.default.backup
+mkdir -p /etc/nginx/conf.d
 cp ./bin/config/nginx.server.conf /etc/nginx/conf.d/default.conf
 cp ./bin/config/nginx.conf        /etc/nginx/nginx.conf
 service nginx restart
@@ -48,3 +40,13 @@ cp ./frontend/express/public/javascripts/countly/countly.config.sample.js ./fron
 cp ./api/config.sample.js ./api/config.js
 cp ./frontend/express/config.sample.js ./frontend/express/config.js
 cp ./plugins/plugins.default.json ./plugins/plugins.json
+
+
+# install plugins
+node ./bin/scripts/install_plugins
+
+# install sdk-web
+./bin/scripts/update.sh sdk-web
+
+# compile scripts for production
+grunt dist-all
