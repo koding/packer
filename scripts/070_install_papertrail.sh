1399ccf2ffd05f26bf3772d1b86e7c30aae3c098
Cihangir SAVAS <cihangir@koding.com>
2017-01-30 16:04:13 +0300

    packer: move remote_syslog init file to data folder

--- scripts/070_install_papertrail.sh
+++ scripts/070_install_papertrail.sh
@@ -11,6 +11,6 @@ mkdir -p /var/log/koding
 
 curl --silent --location $REMOTE_SYSLOG_TARBALL_URL | tar --verbose --extract --gzip --directory=/usr/local
 
-# TODO WHERE IS INIT>D FILE???
+cp $(dirname $0)/data/remote_syslog.init /etc/rc.d/init.d/remote_syslog
 
 chkconfig --add remote_syslog
