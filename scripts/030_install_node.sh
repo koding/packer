b310797bb938935b39d5a9c71ba12d2ec3983d44
Cihangir SAVAS <cihangir@koding.com>
2017-02-02 01:07:23 +0300

    packer/scripts: fix env vars

--- scripts/030_install_node.sh
+++ scripts/030_install_node.sh
@@ -10,9 +10,15 @@ curl --silent $NODE_URL | tar --extract --xz --directory /usr/local
 ln --symbolic /usr/local/$NODE_NAME/bin/* /usr/local/bin/
 ln --symbolic /usr/local/bin/node /usr/local/bin/nodejs
 
-[[ ":$PATH:" != */usr/local/bin* ]] && PATH="/usr/local/bin:${PATH}"
+[[ ":$PATH:" != */usr/local/bin* ]] &&  echo 'export PATH="/usr/local/bin:${PATH}"' >>/etc/profile.d/local_bin.sh
+[[ ":$PATH:" != */usr/local/share/npm/bin* ]] && echo 'export PATH="/usr/local/share/npm/bin:${PATH}"' >>/etc/profile.d/npm_bin.sh
+
+# for commands below
+source /etc/profile
 
 ## npm
+# set npm to use a global place other than the installation path
+npm config set prefix /usr/local
 
 npm install --global npm@$NPM_VERSION
 
