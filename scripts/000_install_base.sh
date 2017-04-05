d5abae5b7c5cc5c647609dafda5916d1b5a2c2b9
Sonmez Kartal <sonmez@koding.com>
2017-04-05 17:03:57 +0300

    Base installer group script
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

---
+++ scripts/000_install_base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pushd $(dirname $0)
+
+./000_configure_server.sh
+./010_install_utilities.sh
+./020_install_nginx.sh
+./030_install_node.sh
+./040_install_go.sh
+./050_install_pip.sh
+./050_install_supervisor.sh
+./060_install_misc.sh
+./070_install_datadog.sh
+./070_install_papertrail.sh
+./900_cleanup.sh
+
+popd
+
+exit 0
