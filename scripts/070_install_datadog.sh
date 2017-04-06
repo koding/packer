d9c583bea150bab400d0c1c86801dfad63775032
Sonmez Kartal <sonmez@koding.com>
2017-04-07 00:07:00 +0300

    Fix paths
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/070_install_datadog.sh
+++ scripts/070_install_datadog.sh
@@ -6,20 +6,20 @@ cp $(dirname $0)/data/yum.repos.d/datadog.repo /etc/yum.repos.d/datadog.repo
 # move required files to etc folder.
 cp -R $(dirname $0)/data/dd-agent /etc
 
-mkdir /opt/elasticbeanstalk/hooks/appdeploy/post
 mkdir /opt/elasticbeanstalk/hooks/appdeploy/pre
+mkdir /opt/elasticbeanstalk/hooks/appdeploy/post
 
 cp $(dirname $0)/data/dd-agent/hooks/99stop_datadog.sh /opt/elasticbeanstalk/hooks/appdeploy/pre/
 cp $(dirname $0)/data/dd-agent/hooks/99start_datadog.sh /opt/elasticbeanstalk/hooks/appdeploy/post/
 
-mkdir /opt/elasticbeanstalk/hooks/configdeploy/post
 mkdir /opt/elasticbeanstalk/hooks/configdeploy/pre
+mkdir /opt/elasticbeanstalk/hooks/configdeploy/post
 
 cp $(dirname $0)/data/dd-agent/hooks/99stop_datadog.sh /opt/elasticbeanstalk/hooks/configdeploy/pre/
 cp $(dirname $0)/data/dd-agent/hooks/99start_datadog.sh /opt/elasticbeanstalk/hooks/configdeploy/post/
 
-mkdir /opt/elasticbeanstalk/hooks/post
-mkdir /opt/elasticbeanstalk/hooks/pre
+mkdir /opt/elasticbeanstalk/hooks/preinit
+mkdir /opt/elasticbeanstalk/hooks/postinit
 
 cp $(dirname $0)/data/dd-agent/hooks/99stop_datadog.sh /opt/elasticbeanstalk/hooks/preinit/
 cp $(dirname $0)/data/dd-agent/hooks/99start_datadog.sh /opt/elasticbeanstalk/hooks/postinit
