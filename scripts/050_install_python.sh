81489663eb96f742d1d9c9d22e5881c2e448d639
Sonmez Kartal <sonmez@koding.com>
2017-05-02 13:39:36 +0300

    Install python 2.7
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/050_install_python.sh
+++ scripts/050_install_python.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# INSTALL PYTHON AND SUPERVISORD - on some systems some of the below does not exist.
-yum install python26-pip || echo python26-pip installed
-yum install python-pip || echo python-pip installed
-yum install python-devel || echo python-devel installed
-yum install python-setuptools || echo python-setuptools installed
-
 source /etc/profile
 
-easy_install --upgrade pip==$PIP_VER
+yum install python27 python27-devel python27-pip || :
+
+pip install --upgrade pip==$PIP_VER
