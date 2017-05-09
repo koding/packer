463b311af82566553bec7ad48f60cd8cc911c931
Sonmez Kartal <sonmez@koding.com>
2017-05-10 01:24:22 +0300

    Lookup local IPv4 address via ec2-metadata program
    Signed-off-by: Sonmez Kartal <sonmez@koding.com>

--- scripts/env/host.sh
+++ scripts/env/host.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export HOST_IP=${HOST_IP:-$(curl http://169.254.169.254/latest/meta-data/local-ipv4)}
+export HOST_IP=${HOST_IP:-$(ec2-metadata --local-ipv4 | awk '{print $2}')}
 export PRIVATE_IPV4=${PRIVATE_IPV4:-$HOST_IP}
