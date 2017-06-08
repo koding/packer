9bf17dad3fbb346f8046335a5f23717877c64383
Cihangir SAVAS <cihangir@savas.io>
2017-06-08 17:28:49 +0300

    packer/scripts: export EB_APP_DEPLOY_DIR EB_APP_SUPPORT_DIR  and EB_APP_USER

--- scripts/eb/set_env_vars.sh
+++ scripts/eb/set_env_vars.sh
@@ -10,6 +10,24 @@ if [ -z $EB_ENV_NAME ]; then
 	echo "export EB_ENV_NAME=$EB_ENV_NAME" >>/etc/profile.d/eb_envvars.sh
 fi
 
+if [ -z $EB_APP_DEPLOY_DIR ]; then
+	export EB_APP_DEPLOY_DIR=$(/opt/elasticbeanstalk/bin/get-config container -k app_deploy_dir)
+	[[ $EB_APP_DEPLOY_DIR == "" ]] && echo "EB_APP_DEPLOY_DIR" is empty && exit 1
+	echo "export EB_APP_DEPLOY_DIR=$EB_APP_DEPLOY_DIR" >>/etc/profile.d/eb_envvars.sh
+fi
+
+if [ -z $EB_APP_SUPPORT_DIR ]; then
+	export EB_APP_SUPPORT_DIR=$(/opt/elasticbeanstalk/bin/get-config container -k app_support_dir)
+	[[ $EB_APP_SUPPORT_DIR == "" ]] && echo "EB_APP_SUPPORT_DIR" is empty && exit 1
+	echo "export EB_APP_SUPPORT_DIR=$EB_APP_SUPPORT_DIR" >>/etc/profile.d/eb_envvars.sh
+fi
+
+if [ -z $EB_APP_USER ]; then
+	export EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config container -k app_user)
+	[[ $EB_APP_USER == "" ]] && echo "EB_APP_USER" is empty && exit 1
+	echo "export EB_APP_USER=$EB_APP_USER" >>/etc/profile.d/eb_envvars.sh
+fi
+
 if [ -z $EC2_INSTANCE_ID ]; then
 	export EC2_INSTANCE_ID="$(/opt/aws/bin/ec2-metadata --instance-id | awk '{print $2}')"
 	[[ $EC2_INSTANCE_ID == "" ]] && echo "EC2_INSTANCE_ID" is empty && exit 1
