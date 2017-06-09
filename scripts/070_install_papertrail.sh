2f8e2380bced8194a9be6e092f742b7a24a4e39f
Cihangir SAVAS <cihangir@savas.io>
2017-06-09 10:15:11 +0300

    packer/scripts: link remote_syslog to /usr/local/bin

--- scripts/070_install_papertrail.sh
+++ scripts/070_install_papertrail.sh
@@ -7,6 +7,8 @@ mkdir -p /var/log/koding
 
 curl --silent --location $REMOTE_SYSLOG_TARBALL_URL | tar --verbose --extract --gzip --directory=/usr/local
 
+ln --symbolic /usr/local/remote_syslog/remote_syslog /usr/local/bin/remote_syslog
+
 cp $(dirname $0)/data/remote_syslog.init /etc/rc.d/init.d/remote_syslog
 
 chkconfig --add remote_syslog
